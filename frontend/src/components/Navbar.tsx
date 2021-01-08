f03a382477601891c2888f5f5987d323c11fcc15
justin-chu <justinchu252@gmail.com>
2021-01-08 03:53:08 -0500

    Footer and navbar changes

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -18,12 +18,18 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
   } else if (!data?.me) {
     body = (
       <>
-        <h1 className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-          <NextLink href="/login">Login</NextLink>
-        </h1>
-        <h1 className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-          <NextLink href="/register">Sign Up</NextLink>
-        </h1>
+        <div className="flex space-x-2">
+          <NextLink href="/login">
+            <div className="text-black px-3 py-2 rounded-md text-sm cursor-pointer">
+              Login
+            </div>
+          </NextLink>
+          <NextLink href="/register">
+            <div className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer">
+              Sign Up
+            </div>
+          </NextLink>
+        </div>
       </>
     );
     // user is logged in
@@ -31,91 +37,61 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
     body = (
       <>
         <NextLink href="/create-post">
-          <button
-            type="button"
-            className="py-2 px-4 flex justify-center items-center bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 "
-          >
-            <svg
-              width="20"
-              height="20"
-              fill="currentColor"
-              className="mr-2"
-              viewBox="0 0 1792 1792"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M1344 1472q0-26-19-45t-45-19-45 19-19 45 19 45 45 19 45-19 19-45zm256 0q0-26-19-45t-45-19-45 19-19 45 19 45 45 19 45-19 19-45zm128-224v320q0 40-28 68t-68 28h-1472q-40 0-68-28t-28-68v-320q0-40 28-68t68-28h427q21 56 70.5 92t110.5 36h256q61 0 110.5-36t70.5-92h427q40 0 68 28t28 68zm-325-648q-17 40-59 40h-256v448q0 26-19 45t-45 19h-256q-26 0-45-19t-19-45v-448h-256q-42 0-59-40-17-39 14-69l448-448q18-19 45-19t45 19l448 448q31 30 14 69z"></path>
-            </svg>
+          <div className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer">
             Upload
-          </button>
+          </div>
         </NextLink>
-        <div className="ml-3 relative">
-          <div className="relative inline-block text-left">
-            <div>
-              <button
-                type="button"
-                className="flex items-center justify-center w-full rounded-md  px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-50 hover:bg-gray-50 dark:hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500"
-                id="options-menu"
-                aria-haspopup="true"
-                aria-expanded="true"
-              >
-                <svg
-                  width="20"
-                  fill="currentColor"
-                  height="20"
-                  className="text-gray-800"
-                  viewBox="0 0 1792 1792"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M1523 1339q-22-155-87.5-257.5t-184.5-118.5q-67 74-159.5 115.5t-195.5 41.5-195.5-41.5-159.5-115.5q-119 16-184.5 118.5t-87.5 257.5q106 150 271 237.5t356 87.5 356-87.5 271-237.5zm-243-699q0-159-112.5-271.5t-271.5-112.5-271.5 112.5-112.5 271.5 112.5 271.5 271.5 112.5 271.5-112.5 112.5-271.5zm512 256q0 182-71 347.5t-190.5 286-285.5 191.5-349 71q-182 0-348-71t-286-191-191-286-71-348 71-348 191-286 286-191 348-71 348 71 286 191 191 286 71 348z"></path>
-                </svg>
-              </button>
-            </div>
-            <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5">
-              <div
-                className="py-1 "
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="options-menu"
-              >
-                <div
-                  className="block block px-4 py-2 text-md text-gray-700 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600"
-                  role="menuitem"
-                >
-                  <span className="flex flex-col text-gray-400 font-light">
-                    <span>Hi, {data.me.username}!</span>
-                  </span>
-                </div>
-                <a
-                  href="#"
-                  className="block block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600"
-                  role="menuitem"
-                >
-                  <span className="flex flex-col">
-                    <span>Settings</span>
-                  </span>
-                </a>
-                <a
-                  href="#"
-                  className="block block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600"
-                  role="menuitem"
-                >
-                  <span className="flex flex-col">
-                    <span>Account</span>
-                  </span>
-                </a>
-                <button
-                  onClick={async () => {
-                    await logout();
-                    router.reload();
-                  }}
-                  className="block block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600"
-                  role="menuitem"
-                >
-                  <span className="flex flex-col">
-                    <span>Logout</span>
-                  </span>
-                </button>
-              </div>
+        <div className="ml-5 relative">
+          <div>
+            <button
+              className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              id="user-menu"
+              aria-haspopup="true"
+            >
+              <span className="sr-only">Open user menu</span>
+              <img
+                className="h-8 w-8 rounded-full"
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+            </button>
+          </div>
+          <div
+            className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="user-menu"
+          >
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              role="menuitem"
+            >
+              Profile
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              role="menuitem"
+            >
+              Notifications
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              role="menuitem"
+            >
+              Settings
+            </a>
+            <div
+              onClick={async () => {
+                await logout();
+                router.reload();
+              }}
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"
+              role="menuitem"
+            >
+              Sign out
             </div>
           </div>
         </div>
@@ -123,78 +99,109 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
     );
   }
   return (
-    <nav className="bg-white dark:bg-gray-800 shadow top-0 sticky z-50">
-      <div className="max-w-7xl mx-auto px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className=" flex items-center">
-            <a className="flex-shrink-0" href="/">
-              <img
-                className="h-8 w-8"
-                src="https://dummyimage.com/600x500/F3F4F7/64748b"
-                alt="Workflow"
-              />
-            </a>
-            <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
-                <h1 className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-                  <NextLink href="/">Home</NextLink>
-                </h1>
-                <h1 className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-                  <NextLink href="/login">Login</NextLink>
-                </h1>
-                <h1 className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-                  <NextLink href="/register">Sign Up</NextLink>
-                </h1>
-                <h1 className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-                  <NextLink href="/">Home</NextLink>
-                </h1>
-              </div>
-            </div>
-          </div>
-          <div className="block">
-            <div className="ml-4 flex items-center md:ml-6">{body}</div>
-          </div>
-          <div className="-mr-2 flex md:hidden">
-            <button className="text-gray-800 hover:text-gray-400 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none">
+    <nav className="bg-white">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-500 hover:text-black focus:outline-none focus:text-black"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
               <svg
-                width="20"
-                height="20"
-                fill="currentColor"
-                className="h-8 w-8"
-                viewBox="0 0 1792 1792"
+                className="block h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
               >
-                <path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+              <svg
+                className="hidden h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
               </svg>
             </button>
           </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center">
+              <img
+                className="block lg:hidden h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                alt="Workflow"
+              />
+              <img
+                className="hidden lg:block h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                alt="Workflow"
+              />
+            </div>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                <a
+                  href="#"
+                  className="bg-gray-100 text-black px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Explore
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-500 hover:bg-gray-100 hover:text-black px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Developers
+                </a>
+                {/* <a
+                  href="#"
+                  className="text-gray-500 hover:bg-gray-100 hover:text-black px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Market
+                </a> */}
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            {body}
+          </div>
         </div>
       </div>
-      <div className="md:hidden">
-        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-          <NextLink href="/">
-            <h1 className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-              Home
-            </h1>
-          </NextLink>
+
+      <div className="hidden sm:hidden">
+        <div className="px-2 pt-2 pb-3 space-y-1">
           <a
-            className="text-gray-800 dark:text-white block px-3 py-2 rounded-md text-base font-medium"
-            href="/#"
+            href="#"
+            className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
           >
-            Gallery
+            Discover
           </a>
           <a
-            className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            href="/#"
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
           >
-            Content
+            Developers
           </a>
-          <a
-            className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            href="/#"
+          {/* <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
           >
-            Contact
-          </a>
+            Market
+          </a> */}
         </div>
       </div>
     </nav>
